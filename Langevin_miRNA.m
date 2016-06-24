c5f35b4a88d27ce571053ac6753c1b78916c5e5b
Rakshit <Rakshit@Rakshits-MacBook-Pro.local>
2016-06-23 22:09:09 -0700

    Code for Langevin Simulations

---
+++ Langevin_miRNA.m
@@ -0,0 +1,34 @@
+% Function for Simulating Langevin Equations for miRNA Regulation for Gene Expression
+function LS_miRNA(mi, p, r, K_r, K_p, K_mi,  K_t, Y_r, Y_p, Y_mi, h, n)
+	R(1) = r;
+	P(1) = p;
+	Mi(1) = mi;
+	for i = 1:n+1
+		% Simulating over the step 
+		% Generating Random Noise for various noise terms in Langevin Simulations
+		X = randn();
+		R(i+1)  =  R(i) + h(K_r - K_p*R(i) - K_t*R(i)*Mi(i) - Y_r*R(i) + sqrt(K_p*R(i))*X(0) + sqrt(K_t*R(i)*Mi(i))* X(1) + sqrt(Y_r*R(i))* X(2) ); 
+		P(i+1)  =  P(i) + h(K_p*R(i) - Y_p*P(i)	+ sqrt(K_p*R(i))*X(0) + 	sqrt(Y_p*P(i))	);
+		Mi(i+1) =  M(i) + h(K_mi  - K_t*R(i)*Mi(i)	- Y_mi* Mi(i) + sqrt(K_t*R(i)*Mi(i))* X(1)	+ sqrt(Y_mi*Mi(i))*X(2)	); 
+	
+	end
+% Time Axis for plotting	
+	t = 0 :h:n*h
+
+	
+%	plot()
+%	xlabel()
+%	ylabel()
+	
+%	plot()
+%	xlabel()
+%	ylabel()
+
+end
+
+% Function for simulating Gillespie Algorithm for miRNA Regulation for Gene Expression
+
+%function GS_miRNA(mi, p, r, K_r, K_p, K_t, Y_r, Y_p, Y_mi, h, n)
+
+
+
