8a875cb16a91bb887915e3f27542be98fe5a612a
rj2808 <rakshit28081996@gmail.com>
2016-06-26 12:47:41 +0530

    Changes in Langevin_miRNA

--- Langevin_miRNA.m
+++ Langevin_miRNA.m
@@ -4,26 +4,32 @@ function LS_miRNA(mi, p, r, K_r, K_p, K_mi,  K_t, Y_r, Y_p, Y_mi, h, n)
 	P(1) = p;
 	Mi(1) = mi;
 	for i = 1:n+1
+        X = randn()
+        Y = randn()
+        Z = randn() 
 		% Simulating over the step 
 		% Generating Random Noise for various noise terms in Langevin Simulations
-		R(i+1)  =  R(i) + h*(K_r - (K_p*R(i)) - (K_t*(R(i))*(Mi(i))) - (Y_r*(R(i))) + (sqrt(K_p*(R(i)))*(randn)) + (sqrt(K_t*(R(1))*(Mi(1)))*randn) + (sqrt(Y_r*(R(1)))* randn) ); 
-		P(i+1)  =  P(i) + h*(K_p*R(i) - Y_p*P(i)	+ sqrt(K_p*R(i))*randn + 	sqrt(Y_p*P(i))*randn	);
-		Mi(i+1) =  Mi(i) + h*(K_mi  - K_t*R(i)*Mi(i)	- Y_mi* Mi(i) + sqrt(K_t*R(i)*Mi(i))* randn	+ sqrt(Y_mi*Mi(i))*randn	); 
+		R(i+1)  =  R(i) + h*(K_r - (K_p*R(i)) - (K_t*(R(i))*(Mi(i))) - (Y_r*(R(i))) + (sqrt(K_p*(R(i)))*(X)) + (sqrt(K_t*(R(1))*(Mi(1)))*Y) + (sqrt(Y_r*(R(1)))* Z) ); 
+		P(i+1)  =  P(i) + h*(K_p*R(i) - Y_p*P(i)	+ sqrt(K_p*R(i))*X + 	sqrt(Y_p*P(i))*Z	);
+		Mi(i+1) =  Mi(i) + h*(K_mi  - K_t*R(i)*Mi(i)	- Y_mi* Mi(i) + sqrt(K_t*R(i)*Mi(i))* Y	+ sqrt(Y_mi*Mi(i))*Z	); 
 	
-	end
+    end
 % Time Axis for plotting	
 	t = 0 :h:(n+1)*h
     plot(t,P)
+    title('K_p',num2str(K_p),'K_r',num2str(K_r),'K_mi',num2str(K_mi),'K_t',num2str(K_t),'Y_r',num2str(Y_r),'Y_p',num2str(Y_p),'Y_mi',num2str(Y_mi))
 	xlabel('Time')
 	ylabel('Protien Concerntration')
     figure()
-
+ 
 	plot(t,R)
+    title('K_p',num2str(K_p),'K_r',num2str(K_r),'K_mi',num2str(K_mi),'K_t',num2str(K_t),'Y_r',num2str(Y_r),'Y_p',num2str(Y_p),'Y_mi',num2str(Y_mi))
 	xlabel('Time')
 	ylabel('mRNA Concerntration') 
     figure()
 
     plot(t,Mi)
+    title('K_p',num2str(K_p),'K_r',num2str(K_r),'K_mi',num2str(K_mi),'K_t',num2str(K_t),'Y_r',num2str(Y_r),'Y_p',num2str(Y_p),'Y_mi',num2str(Y_mi))
     xlabel('Time')
     ylabel('miRNA Concerntration')
 
