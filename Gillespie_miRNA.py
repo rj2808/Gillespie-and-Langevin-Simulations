317ad23889cd92d30096176291e64f8797be9555
Rakshit <rakshit28081996@gmail.com>
2016-07-11 00:54:54 +0530

    Some Changes

--- Gillespie_miRNA.py
+++ Gillespie_miRNA.py
@@ -1,8 +1,29 @@
 import random
 import math
+import stochpy
 
 __author__ = 'Rakshit'
 
 class Gillespie()
+		# Initialising the Parameters
 		time,molA,molB = 0,0,0
-		
+		h,j = 0,0
+		ka,kKa,kb,kKb = 0,0,0,0
+		da, db = 0,0
+		prop = []
+		n_prop = []
+		p_sum = 0
+
+		# Applying the Stocastic Algorithm
+		def Algorithm_for (self, Rate)
+			while self.molA >= 0		# Loop Until the Value of A is zero
+					propensity = (self.molA * self.molB) * rate
+
+					if propensity == 0
+							return 0
+					self.molA -= 1
+
+					self.time +=  
+
+
+
