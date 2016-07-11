d9b849437da40ae34203efb78154f587dd0fc70d
Rakshit <rakshit28081996@gmail.com>
2016-07-11 19:13:25 +0530

    Working Code for Gillespie of A + B -> B

--- Gillespie_miRNA.py
+++ Gillespie_miRNA.py
@@ -5,7 +5,7 @@ import matplotlib as plt
 
 __author__ = 'Rakshit'
 
-class Gillespie()
+class Gillespie :
 		# Initialising the Parameters
 		time,molA,molB = 0,0,0
 		h,j = 0,0
@@ -17,14 +17,22 @@ class Gillespie()
 
 		# Applying the Stocastic Algorithm
 		# For the Reaction A + B -> A
-		def Algorithm_for_Simulation (self, Rate)
-			while self.molA >= 0		# Loop Until the Value of A is zero
-					propensity = (self.molA * self.molB) * rate
+		def Algorithm_for_Simulation(self, Rate) :
+			while self.molA >= 0 :
+					propensity = (self.molA * self.molB) * Rate
 
-					if propensity == 0
-							return 0
+					if propensity == 0:
+						 return 0
 					self.molA -= 1
 
 					self.time +=  (-1/propensity)*math.log(random.random()) # Calculating Time Increment
 
 					print(self.time,self.molA/(self.molA +self.molB),self.molB/(self.molA + self.molB)) # Prining out the Arguements
+
+		# Function used to run the simulations
+		def run(self,a,b,Rate) :
+				self.molA = a
+				self.molB = b
+				self.Algorithm_for_Simulation(Rate)
+G = Gillespie()
+G.run(19999,1,1)
