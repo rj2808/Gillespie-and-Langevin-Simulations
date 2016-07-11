ffde6eccc87d23eaa4051da91ad73c8ec4c796d5
Rakshit <rakshit28081996@gmail.com>
2016-07-11 20:18:51 +0530

    Working

--- Gillespie_miRNA.py
+++ Gillespie_miRNA.py
@@ -1,7 +1,8 @@
-import random
+from random import random
 import math
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from pylab import *
 
 __author__ = 'Rakshit'
 
@@ -18,6 +19,8 @@ class Gillespie :
 		# Applying the Stocastic Algorithm
 		# For the Reaction A + B -> A
 		def Algorithm_for_Simulation(self, Rate) :
+			plt.ion()
+			plt.axis([0,12,0,self.molA])
 			while self.molA >= 0 :
 					propensity = (self.molA * self.molB) * Rate
 
@@ -25,9 +28,13 @@ class Gillespie :
 						 return 0
 					self.molA -= 1
 
-					self.time +=  (-1/propensity)*math.log(random.random()) # Calculating Time Increment
+					self.time +=  (-1/propensity)*math.log(random()) # Calculating Time Increment
 
 					print(self.time,self.molA/(self.molA +self.molB),self.molB/(self.molA + self.molB)) # Prining out the Arguements
+					plt.scatter(self.time, self.molA)
+
+
+
 
 		# Function used to run the simulations
 		def run(self,a,b,Rate) :
@@ -35,4 +42,6 @@ class Gillespie :
 				self.molB = b
 				self.Algorithm_for_Simulation(Rate)
 G = Gillespie()
-G.run(19999,1,1)
+G.run(20000,1,1)
+plt.show()
+plt.pause(2**31-1)
